41fa3c46de758bd14b30051c199b47790169b2cf
Coder-pro1 <sithijaseneviratne@gmail.com>
2026-02-05 12:26:50 +0530

    requirements install

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+   name="Medical_chatbot",
+   version="1.0",
+   author="Sithija",
+   author_email="sithijaseneviratne@gmail.com",
+   packages=find_packages(),
+   install_requires=[]
+)
